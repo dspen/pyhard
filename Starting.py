9249f7d181367b98e578ebe2f471e58b348659f9
dspen <spencer.darylt@gmail.com>
2018-05-24 09:26:45 -0600

    Initial commit

---
+++ Starting.py
@@ -0,0 +1,49 @@
+#%% Ex1
+# print("Hello")
+# print("Hello Again")
+# print("I like typing this.")
+# print("This is fun")
+# print('Yay! Printing.')
+# print("I'd much rather you 'not'")
+# print('I "said" do not touch this.')
+#%% Ex3
+# print("I will now count:")
+# print("Hens", 25.0 + 30.0/6)
+# print("Roosters", 100 - 25 * 3 % 4)
+# print(3+2+1-5+4%2-1/4+6)
+# print (3+2 < 5-7)
+# print("3+2", 3+2)
+# print("5-7", 5-7)
+# print("Is it greater", 5>-2)
+# print("Is it greater or equal", 5>=-2)
+# print("Is it less or equal", 5<=-2)
+#%% Ex5
+# my_name='Daryl'
+
+# print(f"Hi I'm {my_name}.")
+# print("." * 10) #10 periods
+# print('a'+'b', end='...') #define ending string
+
+#%%
+# formatter = '{} {} {} {}'
+# print(formatter.format('\n',2,False,'one')) #call string method
+#%%
+print("""
+Hi
+2"
+3""
+""") #multi-line print, can use quotes inside
+#%% Ex10
+tabby = "\tI'm tabbed in"
+persian = "I'm split\non a line"
+backslash = "I'm \\ a \\ cat."
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+print(tabby)
+print(persian)
+print(backslash)
+print(fat_cat)
